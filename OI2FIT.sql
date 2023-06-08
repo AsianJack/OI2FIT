c5aae205d466714fe75e5d2f47a9ef07aa4cdea0
Ramon Crivelari <87196771+Racrivelari@users.noreply.github.com>
2023-06-08 20:54:57 -0300

    Update OI2FIT.sql

--- OI2FIT.sql
+++ OI2FIT.sql
@@ -56,14 +56,13 @@ CREATE TABLE IF NOT EXISTS Logs_Colaborador  (
 	constraint fk_Colaborador_log foreign key (id_log) references Aluno(id_colab)
 );
 
-
-INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu) VALUES ('Guilherme', '09853680974', 'Masculino', '18', 'gui@gmail.com', '2022-09-20'); 
-INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu) VALUES ('Jonas', '12345678901', 'Masculino', '20', 'jon@gmail.com', '2022-12-25'); 
-INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu) VALUES ('Michelle', '11111111113', 'Feminino', '22', 'michelle@gmail.com', '2022-10-24'); 
-INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu) VALUES ('Laura', '12398787494', 'Feminino', '13', 'laulau@gmail.com', '2022-11-27'); 
-
-INSERT INTO Colaborador (nome_colab, login_colab, senha_colab, cpf_colab) VALUES ('Lucas', 'lucas', '1', '12312312354'); 
 INSERT INTO Colaborador (nome_colab, login_colab, senha_colab, cpf_colab) VALUES ('Administrador', 'adm', '86f65e28a754e1a71b2df9403615a6c436c32c42a75a10d02813961b86f1e428', '12312312312'); 
+INSERT INTO Colaborador (nome_colab, login_colab, senha_colab, cpf_colab) VALUES ('Administrador2', 'adm2', '86f65e28a754e1a71b2df9403615a6c436c32c42a75a10d02813961b86f1e428', '12312312333'); 
+
+INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu, id_colab) VALUES ('Guilherme', '09853680974', 'Masculino', '18', 'gui@gmail.com', '2022-09-20', 1); 
+INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu, id_colab) VALUES ('Jonas', '12345678901', 'Masculino', '20', 'jon@gmail.com', '2022-12-25', 1); 
+INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu, id_colab) VALUES ('Michelle', '11111111113', 'Feminino', '22', 'michelle@gmail.com', '2022-10-24', 1); 
+INSERT INTO Aluno (nome_alu, cpf_alu, sexo_alu, idade_alu, email_alu, data_matricula_alu, id_colab) VALUES ('Laura', '12398787494', 'Feminino', '13', 'laulau@gmail.com', '2022-11-27', 1); 
 
 INSERT INTO Medidas (peso_kg, altura_m, imc, id_alu) VALUES ('70', '1.70', '25', '4'); 
 INSERT INTO Medidas (peso_kg, altura_m, imc, id_alu) VALUES ('65', '1.70', '24', '4');
